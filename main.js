f8b150ecb29dbbf96c9e9617eef7370f2a080fec
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-11-16 22:11:57 -0500

    Add variables to part1 js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
-
+var section = document.getElementsByTagName('section');
+var button = document.getElementsByTagName('button');
 
 
 
