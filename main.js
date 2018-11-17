477cb444b947ff95e220359216eac435911739f8
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-11-16 22:40:23 -0500

    Commit out part1 code, clear for part2 code

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
-var section = document.getElementsByTagName('section')[0];
-var button = document.getElementsByTagName('button')[0];
+//var section = document.getElementsByTagName('section')[0];
+//var button = document.getElementsByTagName('button')[0];
 
-function slideToggle() {
-	section.classList.toggle('hide');
-};
+//function slideToggle() {
+//	section.classList.toggle('hide');
+//};
 
-
-button.onclick = slideToggle;
+//button.onclick = slideToggle;
 
 
 
