51d98b51bbed6f55ea281150391193c28aed58ae
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-11-16 22:38:51 -0500

    Add event listener and fix any errors in part1

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
-var section = document.getElementsByTagName('section');
-var button = document.getElementsByTagName('button');
+var section = document.getElementsByTagName('section')[0];
+var button = document.getElementsByTagName('button')[0];
 
 function slideToggle() {
 	section.classList.toggle('hide');
-}
+};
+
+
+button.onclick = slideToggle;
 
 
 
