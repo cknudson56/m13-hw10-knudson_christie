2821a663e6d255fde91f65e5bf12c5f124e43a23
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-11-16 22:46:46 -0500

    Add jQuery code to js file

--- main.js
+++ main.js
@@ -11,3 +11,8 @@
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
+$(document).ready(function(){
+	$('button').click(function() {
+		$('section').slideToggle();
+	});
+});
